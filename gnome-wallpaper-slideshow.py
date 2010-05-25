45e3da2653256a56da43e9691b0a5d5014085d36
birbeck <birbeck@8ab36cc2-94ad-8d63-c2bf-ecbb5e3a03e5>
2010-05-25 10:00:25 +0000

    - Added license - Save preferences on exit - Refactored to use libxml2 - Fixed rescaling bug - Write background xml to .gnome-wallpaper-slideshow instead of pictures folder - Do not add duplicate entries to gnomes background.xml

--- gnome-wallpaper-slideshow.py
+++ gnome-wallpaper-slideshow.py
@@ -20,11 +20,15 @@
 import os
 import gtk, gobject
 import gconf
-import xml.dom.minidom
+import libxml2
 
 class GTK_Main:
     
     def __init__(self):
+        self.conf_dir_path = os.path.expanduser("~/.gnome-wallpaper-slideshow/")
+        self.conf_file_path = os.path.join(self.conf_dir_path, "gnome-wallpaper-slideshow.config")
+        self.xml_file_path = os.path.join(self.conf_dir_path, "gnome-wallpaper-slideshow.xml")
+
         # Create GTK Window
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_title("Gnome Wallpaper Slideshow")
@@ -84,6 +88,50 @@ class GTK_Main:
         hbox.pack_end(self.ok_button, False)
         hbox.pack_end(self.cancel_button, False)
         vbox.pack_end(hbox, False, False, 10)
+        
+        # Read settings
+        try:
+            settings = {}
+            conf_file = open(self.conf_file_path, "r")
+            for line in conf_file.readlines():
+                (key, var) = line.strip().split(" ")
+                settings[key] = var
+    
+            if settings['Folder']:
+                self.folder_entry.set_text(settings['Folder'])
+                self.files = []
+                accepted = ('.jpg', '.jpeg', '.gif', '.png')
+                for file in os.listdir(settings['Folder']):
+                    if os.path.splitext(file)[1] in accepted:
+                        self.files.append(os.path.join(settings['Folder'], file))
+                if len(self.files):
+                    self.ok_button.set_sensitive(True)
+    
+            picture_duration = float(settings['PictureDuration'])
+            if picture_duration % 3600 == 0:
+                self.pdur_spinner.set_value(picture_duration / 3600)
+                self.pdur_combo.set_active(0)
+            elif picture_duration % 60 == 0:
+                self.pdur_spinner.set_value(picture_duration / 60)
+                self.pdur_combo.set_active(1)
+            else:
+                self.pdur_spinner.set_value(picture_duration)
+                self.pdur_combo.set_active(2)
+    
+            transition_duration = float(settings['TransitionDuration'])
+            if  transition_duration % 3600 == 0:
+                self.tdur_spinner.set_value(transition_duration / 3600)
+                self.tdur_combo.set_active(0)
+            elif transition_duration % 60 == 0:
+                self.tdur_spinner.set_value(transition_duration / 60)
+                self.tdur_combo.set_active(1)
+            else:
+                self.tdur_spinner.set_value(transition_duration)
+                self.tdur_combo.set_active(2)
+        except IOError:
+            pass
+        except RuntimeError as strerror:
+            print "Failed to parse config file: strerror"
 
         # Show entire window
         self.window.add(vbox)
@@ -94,7 +142,10 @@ class GTK_Main:
                                        (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
                                         gtk.STOCK_OK, gtk.RESPONSE_OK))
         dialog.set_default_response(gtk.RESPONSE_OK)
-        dialog.set_filename(os.path.expanduser("~/Pictures/*"))
+        if self.folder_entry.get_text():
+            dialog.set_filename(self.folder_entry.get_text() + "/*")
+        else:
+            dialog.set_filename(os.path.expanduser("~/Pictures/*"))
         response = dialog.run()
         filename = dialog.get_filename()
         dialog.destroy()
@@ -110,6 +161,7 @@ class GTK_Main:
             if len(self.files):
                 self.ok_button.set_sensitive(True)
             else:
+                self.ok_button.set_sensitive(False)
                 dialog = gtk.MessageDialog(parent = None, flags = gtk.DIALOG_DESTROY_WITH_PARENT,
                     type = gtk.MESSAGE_ERROR, buttons = gtk.BUTTONS_OK,
                     message_format = "Folder contains no images")
@@ -119,10 +171,10 @@ class GTK_Main:
         
     def ok_button_clicked(self, button):
         nfiles = len(self.files)
-        
         if nfiles <= 0:
             return False
         
+        # Read settings
         folder = self.folder_entry.get_text()
 
         picture_duration = int(self.pdur_spinner.get_text())
@@ -136,111 +188,90 @@ class GTK_Main:
             transition_duration = transition_duration * 60
         elif self.tdur_combo.get_active_text() == "hr":
             transition_duration = transition_duration * 60 * 60
-                
-        document = """<background><starttime><year>2009</year><month>08</month><day>04</day><hour>00</hour><minute>00</minute><second>00</second></starttime></background>"""
-        dom = xml.dom.minidom.parseString(document)
+         
+        # Save settings
+        if not os.path.exists(self.conf_dir_path):
+            os.mkdir(self.conf_dir_path, 0755)
+
+        settings = "Folder %s\nPictureDuration %d\nTransitionDuration %d\n" % (
+            folder, picture_duration, transition_duration)
+        conf_file = open(self.conf_file_path, "w")
+        conf_file.write(settings)
+        conf_file.close()
+
+        # Create wallpaper xml
+        doc = libxml2.parseDoc("""<background><starttime><year>2010</year><month>01</month><day>01</day><hour>00</hour><minute>00</minute><second>00</second></starttime></background>""")
+        root = doc.getRootElement()
         
         for i in range(0, nfiles):
-            static = dom.createElement("static")
-            duration = dom.createElement("duration")
-            duration.appendChild(dom.createTextNode(str(picture_duration)))
-            static.appendChild(duration)
-            file = dom.createElement("file")
-            file.appendChild(dom.createTextNode(self.files[i]))
-            static.appendChild(file)
-            dom.documentElement.appendChild(static)
+            static = root.newChild(None, "static", None)
+            static.addChild(static.newChild(None, "duration", str(picture_duration)))
+            static.addChild(static.newChild(None, "file", self.files[i]))
+            root.addChild(static)
 
             if transition_duration <= 0: continue
- 
-            transition = dom.createElement("transition")
-            duration = dom.createElement("duration")
-            duration.appendChild(dom.createTextNode(str(transition_duration)))
-            transition.appendChild(duration)
-            frm = dom.createElement("from")
-            frm.appendChild(dom.createTextNode(self.files[i]))
-            transition.appendChild(frm)
-            to = dom.createElement("to")
-            if i < nfiles-1:
-                to.appendChild(dom.createTextNode(self.files[i+1]))
-            else:
-                to.appendChild(dom.createTextNode(self.files[0]))
-            transition.appendChild(to)
-            dom.documentElement.appendChild(transition)
-            
-        try:
-            save_filename = os.path.join(folder, "background-1.xml")
-            f = open(save_filename, 'w')
-            f.write(dom.toxml("utf-8"))
-            f.close()
-        except IOError as (errno, strerror):
-            try:
-                save_filename = os.path.join(os.path.expanduser("~"), ".dynamic-wallpaper.xml")
-                f = open(save_filename, 'w')
-                f.write(dom.toxml("utf-8"))
-                f.close()
-            except Error as (errno, strerror):
-                dialog = gtk.MessageDialog(parent = None, flags = gtk.DIALOG_DESTROY_WITH_PARENT,
-                    type = gtk.MESSAGE_ERROR, buttons = gtk.BUTTONS_OK,
-                    message_format = strerror)
-                dialog.set_title('Error')
-                dialog.connect('response', lambda dialog, response: dialog.destroy())
-                dialog.run()
-                return False
-        except Error as (errno, strerror):
-                dialog = gtk.MessageDialog(parent = None, flags = gtk.DIALOG_DESTROY_WITH_PARENT,
-                    type = gtk.MESSAGE_ERROR, buttons = gtk.BUTTONS_OK,
-                    message_format = strerror)
-                dialog.set_title('Error')
-                dialog.connect('response', lambda dialog, response: dialog.destroy())
-                dialog.run()
-                return False
-        
-        backgrounds_file = os.path.join(os.path.expanduser("~/.gnome2/"), "backgrounds.xml")
-        doc = xml.dom.minidom.parse(backgrounds_file)
-        
-        wallpaper = dom.createElement("wallpaper")
-        wallpaper.setAttribute("deleted", "false")
-        name = dom.createElement("name")
-        name.appendChild(dom.createTextNode("Dynamic Wallpaper"))
-        filename = dom.createElement("filename")
-        filename.appendChild(dom.createTextNode(save_filename))
-        options = dom.createElement("options")
-        options.appendChild(dom.createTextNode("stretched"))
-        shade_type = dom.createElement("shade_type")
-        shade_type.appendChild(dom.createTextNode("solid"))
-        pcolor = dom.createElement("pcolor")
-        pcolor.appendChild(dom.createTextNode("#2c2c00001e1e"))
-        scolor = dom.createElement("scolor")
-        scolor.appendChild(dom.createTextNode("#2c2c00001e1e"))
-        wallpaper.appendChild(name)
-        wallpaper.appendChild(filename)
-        wallpaper.appendChild(options)
-        wallpaper.appendChild(shade_type)
-        wallpaper.appendChild(pcolor)
-        wallpaper.appendChild(scolor)
-        doc.documentElement.appendChild(wallpaper)
 
+            transition = root.newChild(None, "transition", None)
+            transition.addChild(transition.newProp("type", "overlay"))
+            transition.addChild(transition.newChild(None, "duration", str(transition_duration)))
+            transition.addChild(transition.newChild(None, "from", self.files[i]))
+            if i < nfiles-1:
+                transition.addChild(transition.newChild(None, "to", self.files[i+1]))
+            else:
+                transition.addChild(transition.newChild(None, "to", self.files[0]))
+            root.addChild(transition)
+            
+        # Write XML file
         try:
-            f = open(backgrounds_file, 'w')
-            f.write(doc.toxml("utf-8"))
-            f.close()
-        except Error as (errno, strerror):
-                dialog = gtk.MessageDialog(parent = None, flags = gtk.DIALOG_DESTROY_WITH_PARENT,
-                    type = gtk.MESSAGE_ERROR, buttons = gtk.BUTTONS_OK,
-                    message_format = strerror)
-                dialog.set_title('Error')
-                dialog.connect('response', lambda dialog, response: dialog.destroy())
-                dialog.run()
-                return False
+            doc.saveFormatFileEnc(self.xml_file_path, "utf-8", 1)
+        except IOError as (errno, strerror):
+            dialog = gtk.MessageDialog(parent = None, flags = gtk.DIALOG_DESTROY_WITH_PARENT,
+                type = gtk.MESSAGE_ERROR, buttons = gtk.BUTTONS_OK,
+                message_format = strerror)
+            dialog.set_title('Error')
+            dialog.connect('response', lambda dialog, response: dialog.destroy())
+            dialog.run()
+            return False
         
+        # Parse gnome backgrounds.xml file
+        backgrounds_file = os.path.expanduser("~/.gnome2/backgrounds.xml")
+        doc = libxml2.parseFile(backgrounds_file)
+        root = doc.getRootElement()
+        
+        # Look for existing wallpaper entry
+        ctxt = doc.xpathNewContext()
+        res = ctxt.xpathEval("/wallpapers/wallpaper[filename=\""+self.xml_file_path+"\"]")
+        
+        # If no entry exists, add one
+        if len(res) == 0:
+            wallpaper = root.newChild(None, "wallpaper", None)
+            wallpaper.addChild(wallpaper.newProp("deleted", "false"))
+            wallpaper.addChild(wallpaper.newChild(None, "name", "gnome-wallpaper-slideshow"))
+            wallpaper.addChild(wallpaper.newChild(None, "filename", self.xml_file_path))
+            wallpaper.addChild(wallpaper.newChild(None, "options", "stretched"))
+            wallpaper.addChild(wallpaper.newChild(None, "shade_type", "solid"))
+            wallpaper.addChild(wallpaper.newChild(None, "pcolor", "#2c2c00001e1e"))
+            wallpaper.addChild(wallpaper.newChild(None, "scolor", "#2c2c00001e1e"))
+            root.addChild(wallpaper)
+
+            try:
+                doc.saveFormatFileEnc(backgrounds_file, "utf-8", 1)
+            except IOError as (errno, strerror):
+                    dialog = gtk.MessageDialog(parent = None, flags = gtk.DIALOG_DESTROY_WITH_PARENT,
+                        type = gtk.MESSAGE_ERROR, buttons = gtk.BUTTONS_OK,
+                        message_format = strerror)
+                    dialog.set_title('Error')
+                    dialog.connect('response', lambda dialog, response: dialog.destroy())
+                    dialog.run()
+                    return False
+        
+        # Set xml file as current background
         conf_client = gconf.client_get_default()
         conf_client.add_dir("/desktop/gnome/background", gconf.CLIENT_PRELOAD_NONE)
-        conf_client.set_string("/desktop/gnome/background/picture_filename", save_filename)
-        conf_client.set_string("/desktop/gnome/background/picture_options", "strecthed")
-        conf_client.set_string("/desktop/gnome/background/color_shading_type", "solid")
-        conf_client.set_string("/desktop/gnome/background/primary_color", "#2c2c00001e1e")
-        conf_client.set_string("/desktop/gnome/background/secondary_color", "#2c2c00001e1e")
+        conf_client.set_string("/desktop/gnome/background/picture_filename", self.xml_file_path)
+        conf_client.set_string("/desktop/gnome/background/picture_options", "stretched")
 
+        # Exit gtk main loop
         gtk.main_quit()
 
 if __name__ == "__main__":
